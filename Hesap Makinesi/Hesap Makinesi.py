62ff623d4c8c2b7ae1ecc33d0bd6a3a078099aa8
Ege <75607751+ege-psd@users.noreply.github.com>
2021-05-04 18:23:13 +0300

    Create Hesap Makinesi.py
    Python 3.8.2 ile yazılmış hesap makinesidir. If, elif, else kullanılmıştır.

---
+++ Hesap Makinesi/Hesap Makinesi.py
@@ -0,0 +1,43 @@
+# Basit Hesap Makinesi#
+while True:
+    print('Hesap Makinesi İşleme Hazırdır.')
+    try:
+        print('Toplama: 1\nÇıkarma: 2\nÇarpma: 3\nBölme: 4\nÜs Alma: 5')
+
+        islem = int(input('İşlem Seçiniz: '))
+
+        if islem == 1:
+            print('Toplama İşlemini Seçtiniz.')
+            sayi1 = int(input('1. Sayıyı Giriniz: '))
+            sayi2 = int(input('2. Sayıyı Giriniz: '))
+            print('İşlemin Sonucu: ',sayi1 + sayi2)
+
+        elif islem == 2:
+            print('Çıkarma İşlemini Seçtiniz.')
+            sayi1 = int(input('1. Sayıyı Giriniz: '))
+            sayi2 = int(input('2. Sayıyı Giriniz: '))
+            print('İşlemin Sonucu: ',sayi1 - sayi2)
+
+        elif islem == 3:
+            print('Çarpma İşlemini Seçtiniz.')
+            sayi1 = int(input('1. Sayıyı Giriniz: '))
+            sayi2 = int(input('2. Sayıyı Giriniz: '))
+            print('İşlemin Sonucu: ',sayi1 * sayi2)
+
+        elif islem == 4:
+            print('Bölme İşlemini Seçtiniz.')
+            sayi1 = int(input('1. Sayıyı Giriniz: '))
+            sayi2 = int(input('2. Sayıyı Giriniz: '))
+            print('İşlemin Sonucu: ',sayi1 / sayi2)
+
+        elif islem == 5:
+            print('Üs Alma İşlemini Seçtiniz.')
+            sayi1 = int(input('Üssünü Alınmasını İstediğiniz Sayıyı Giriniz: '))
+            sayi2 = int(input('Üssü Giriniz: '))
+            print('İşlemin Sonucu: ',sayi1 ** sayi2)
+
+        else:
+            print('Geçersiz İşlem!')
+
+    except ValueError:
+        print('Hatalı Giriş Yaptınız! Tekrar Deneyiniz.\nSadece Sayı Giriniz.')
